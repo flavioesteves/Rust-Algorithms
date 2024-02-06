d06f615bca4f46f9770a0e616a574b00900b78da
Flavio Esteves <flavioafesteves@gmail.com>
2024-02-06 14:14:35 +0000

    QuickSort added

---
+++ src/main.rs
@@ -0,0 +1,7 @@
+use algorithms::quicksort::quick_sort;
+
+fn main() {
+    let mut arr = vec![3, 9, 6, 18, 11];
+
+    quick_sort(&mut arr);
+}
---
+++ src/quicksort/quicksort.rs
@@ -0,0 +1,43 @@
+pub fn quick_sort(arr: &mut [i32]) {
+    if arr.len() <= 1 {
+        return;
+    }
+    let len = arr.len();
+    let pivot_idx = partition(arr, 0, len - 1);
+    quick_sort(&mut arr[0..pivot_idx]);
+    quick_sort(&mut arr[pivot_idx + 1..len]);
+
+    println!("{:?}", arr);
+}
+
+fn partition(arr: &mut [i32], low: usize, high: usize) -> usize {
+    let pivot = arr[high];
+    let mut idx = low as isize - 1;
+
+    for i in low..high {
+        if arr[i] <= pivot {
+            idx += 1;
+            arr.swap(i, idx as usize);
+        }
+    }
+
+    idx += 1;
+    arr.swap(high, idx as usize);
+    idx as usize
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::quicksort::quick_sort;
+    #[test]
+    fn test_quick_sort() {
+        // Arrange
+        let mut arr = vec![9, 3, 7, 4, 69, 420, 42];
+        // Act
+        quick_sort(&mut arr);
+        // Assert
+
+        println!("{:?}", arr);
+        assert_eq!(arr, vec![3, 4, 7, 9, 42, 69, 420]);
+    }
+}
